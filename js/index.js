05d37a0283b0af3946b611e0fb16767be13eea2b
Mahdi <mahdi.developer1@gmail.com>
2022-06-17 14:20:00 +0430

    refactor project from promises chain to async for fetching data

--- js/index.js
+++ js/index.js
@@ -4,13 +4,14 @@ const container = document.querySelector('.word');
 const searchInput = document.querySelector('.search__input');
 const searchBtn = document.querySelector('.search__btn');
 
-function getWord(word) {
+async function getWord(word) {
   if (word === '') {
     alert('Please enter a word');
   } else {
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-      .then((res) => res.json())
-      .then((data) => {
+    try {
+      let res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+      if (res.status === 200) {
+        let data = await res.json();
         let noun = data[0].phonetics[0].text;
         let audioSrc = data[0].phonetics[0].audio;
         if (!noun) {
@@ -21,27 +22,25 @@ function getWord(word) {
         }
         const audio = new Audio(audioSrc);
         container.innerHTML = `
-          <div class="word__searched">
-            <h1 class="word__name">${word}</h1>
-            <button class="word__sound">
-              <img src="./assets/img/sound.svg" alt="sound">
-            </button>
-          </div>
-          <span class="word__noun">noun ${noun}</span>
-          <p class="word__definition">${data[0].meanings[0].definitions[0].definition}</p>
-        `;
-        const wordSound = document.querySelector('.word__sound')
+                  <div class="word__searched">
+                    <h1 class="word__name">${word}</h1>
+                    <button class="word__sound">
+                      <img src="./assets/img/sound.svg" alt="sound">
+                    </button>
+                  </div>
+                  <span class="word__noun">noun ${noun}</span>
+                  <p class="word__definition">${data[0].meanings[0].definitions[0].definition}</p>
+                `;
+        const wordSound = document.querySelector('.word__sound');
         wordSound.addEventListener('click', () => {
-          audio.play()
-        })
-      })
-      .catch((err) => {
-        if ((err.message = 'Failed to fetch')) {
-          alert('Word not found');
-        } else {
-          alert('Please check your connection');
-        }
-      });
+          audio.play();
+        });
+      } else {
+        alert('Word not found')
+      }
+    } catch {
+      alert('Check your connection');
+    }
   }
 }
 
